975a95fbe30cdc0d10b2afc30814eb38cc10dc61
Jawad-24 <125903216+Jawad-24@users.noreply.github.com>
2024-08-15 00:40:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -151,4 +151,5 @@ def process_video(video_path):
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    app.run(app, debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
